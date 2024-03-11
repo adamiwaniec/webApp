d2569ad7c57e867354fd5d1794b6423b542eb86f
adamiwaniec <adamiwaniec47@gmail.com>
2024-03-11 00:34:42 -0500

    last commit didn't work properly

--- react/src/Users.jsx
+++ react/src/Users.jsx
@@ -1,9 +1,9 @@
 const users = {
-    user1: { password: 'password1', money: 100 },
-    user2: { password: 'password2', money: 150 },
-    user3: { password: 'password3', money: 200 },
-    user4: { password: 'password4', money: 250 },
-    user5: { password: 'password5', money: 300 },
-};
+    user1: { password: "password1", money: 98 },
+    user2: { password: "password2", money: 67 },
+    user3: { password: "password3", money: 100 },
+    user4: { password: "password4", money: 41 },
+    user5: { password: "password5", money: 85 },
+  };
   
-export default users;
+  export default users;
